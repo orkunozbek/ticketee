24d524ae096aee54238723856c547b35985c36f1
Orkun Ozbek <orkunozbek15@gmail.com>
2012-01-02 05:11:07 -0500

    Added title functionality for show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  
+  def title(*params)
+    unless params.empty?
+      content_for :title do
+        (params << "Ticketee").join(" - ") unless params.empty?
+      end
+    end
+  end
 end
